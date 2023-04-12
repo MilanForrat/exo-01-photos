198ae8f5fa5d7a65ecb317303ab9b8d4317c241c
milan.forrat <milan.forrat@gmail.com>
2023-04-12 23:03:25 +0200

    feat: functionnal site

---
+++ script.js
@@ -0,0 +1,11 @@
+// let iframe = document.querySelector("iframe");
+// console.log(iframe);
+
+// let btnIframe = iframe.querySelectorAll("button");
+// console.log(btnIframe);
+
+
+// window.setTimeout(function(){
+//     btnIframe = iframe.querySelectorAll("button");
+//     console.log(btnIframe);
+// }, 3500);
